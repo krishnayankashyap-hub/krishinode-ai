909775ac9a5011162351f5f03eeaf2ff56186383
krishnayan kashyap pathak <krishnayankashyap@gmail.com>
2026-05-24 01:33:47 +0530

    final vercel fix

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return render_template("index.html")
-
-# Vercel needs this
-app = app
+{
+  "version": 2,
+  "builds": [
+    {
+      "src": "./app.py",
+      "use": "@vercel/python"
+    }
+  ],
+  "routes": [
+    {
+      "src": "/(.*)",
+      "dest": "app.py"
+    }
+  ]
+}
